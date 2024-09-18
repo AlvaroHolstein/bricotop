576224e1fce68202900e88e1d1e61f0f2e66c5ed
Alvaro Holstein <adholstein@gmail.com>
2024-09-18 22:34:27 +0100

    create gihuaction without interaction with linode

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,9 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Ata Brunão" />
     </div>
   </header>
-  <nav>
-    <TheWelcome />
-  </nav>
 </template>
 
 <style scoped>
